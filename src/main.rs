187ca1adb5b23be7b43ddd8c8c6f56edac374a5a
Yehor Smoliakov <egorsmkv@gmail.com>
2025-07-31 14:03:33 +0300

    Add trivial algorithm to do the task

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,60 @@
-use image::{ImageBuffer, RgbImage};
-use rayon::prelude::*;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use std::time::Instant;
+
+use image::RgbImage;
+use rayon::prelude::*;
 use video_rs::decode::Decoder;
-use video_rs::Reader;
+use num_traits::cast;
 
 fn main() {
+    let use_seek = false;
+
+    if use_seek {
+        // seems like this method does not work
+        read_by_seeks();
+    } else {
+        read_by_dropping();
+    }
+}
+
+fn read_by_dropping() {
+    tracing_subscriber::fmt::init();
+    video_rs::init().unwrap();
+
+    let start = Instant::now();
+
+    let source = PathBuf::from("video.webm");
+    let mut decoder = Decoder::new(source).expect("failed to create decoder");
+
+    let (width, height) = decoder.size();
+    let fps = f64::from(decoder.frame_rate());
+
+    println!("Width: {width}, height: {height}");
+    println!("FPS: {fps}");
+
+    for (n, frame) in decoder.decode_iter().enumerate() {
+        if !n.is_multiple_of(30) {
+            println!("skipping {n} frame");
+            continue;
+        }
+
+        if let Ok((ts, frame)) = frame {
+            let frame_time = ts.as_secs_f64();
+            println!("Frame time: {frame_time}");
+
+            let rgb = frame.to_owned().into_raw_vec_and_offset().0;
+            let path = PathBuf::from(format!("frames/{n}.png"));
+
+            save_rgb_vec_to_image(&rgb, width, height, path.as_path());
+        } else {
+            break;
+        }
+    }
+
+    println!("Elapsed: {:.2?}", start.elapsed());
+}
+
+fn read_by_seeks() {
     tracing_subscriber::fmt::init();
     video_rs::init().unwrap();
 
@@ -17,18 +66,13 @@ fn main() {
     let fps = f64::from(decoder.frame_rate());
     let duration_time = decoder.duration().unwrap();
     let duration_seconds = duration_time.as_secs_f64();
-
     let (width, height) = decoder.size();
-
-    let reader = Reader::new(Path::new("video.mp4")).unwrap();
-    let stream = reader.best_video_stream_index().unwrap();
-    println!("best_video_stream_index: {stream}");
 
     println!("Width: {width}, height: {height}");
 
     if duration_seconds < 0.0 {
-        let frame_count = decoder.frames().unwrap() as f64;
-        let duration_seconds = if fps > 0.0 { frame_count / fps } else { 0.0 };
+        let frame_count = cast(decoder.frames().unwrap()).unwrap_or(0);
+        let duration_seconds = if fps > 0.0 { f64::from(frame_count) / fps } else { 0.0 };
 
         println!("Frame count: {frame_count}");
         println!("Estimated duration (from frames): {duration_seconds} seconds");
@@ -40,9 +84,11 @@ fn main() {
 
     let mut frames_decoded = Vec::new();
 
-    let duration_in_seconds = duration_seconds.round() as i32;
+    let duration_in_seconds = cast(duration_seconds.ceil()).unwrap_or(0);
+    let fps_c = cast(fps.ceil()).unwrap_or(0);
+
     for second in 0..duration_in_seconds {
-        let frame_number = (f64::from(second) * fps).round() as i64;
+        let frame_number = second * fps_c;
 
         match decoder.seek_to_frame(frame_number) {
             Ok(()) => {
@@ -74,15 +120,14 @@ fn main() {
     let start = Instant::now();
     frames_decoded.par_iter().enumerate().for_each(|(n, rgb)| {
         let path = PathBuf::from(format!("frames/{n}.png"));
-        let rgb_data = rgb.to_vec();
 
-        save_rgb_vec_to_image(rgb_data, width, height, &path);
+        save_rgb_vec_to_image(rgb, width, height, path.as_path());
     });
     println!("Elapsed saving: {:.2?}", start.elapsed());
 }
 
-fn save_rgb_vec_to_image(raw_pixels: Vec<u8>, width: u32, height: u32, path: &PathBuf) {
-    let img_buffer: RgbImage = if let Some(img) = ImageBuffer::from_raw(width, height, raw_pixels) {
+fn save_rgb_vec_to_image(raw_pixels: &[u8], width: u32, height: u32, path: &Path) {
+    let img_buffer: RgbImage = if let Some(img) = RgbImage::from_raw(width, height, raw_pixels.to_vec()) {
         img
     } else {
         eprintln!(
@@ -91,8 +136,8 @@ fn save_rgb_vec_to_image(raw_pixels: Vec<u8>, width: u32, height: u32, path: &Pa
         return;
     };
 
-    match img_buffer.save(&path) {
-        Ok(()) => println!("Image successfully saved to {path:?}"),
+    match img_buffer.save(path) {
+        Ok(()) => println!("Image successfully saved to {}", path.display()),
         Err(e) => eprintln!("Error saving image: {e}"),
     }
 }
